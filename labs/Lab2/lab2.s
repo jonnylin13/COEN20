6ba78b4ce34a6347e49cb184ca40fca8803d9fb1
Jonathan Lin <jonnylin13@gmail.com>
2019-04-24 13:22:03 -0700

    Lab3

--- labs/Lab2/lab2.s
+++ labs/Lab2/lab2.s
@@ -1,19 +1,19 @@
-	.syntax   unified
+  .syntax   unified
   .cpu      cortex-m4
   .text     
   .thumb_func
   .align    2
 
-	.global		Ten32
+  .global		Ten32
 Ten32:
-	// Use ldr to place the constant 10 in R0
+	# Use ldr to place the constant 10 in R0
 	ldr		R0,=10
 	bx		LR
 
 	.global		Ten64
 Ten64:
 	ldr		R0,=10
-	// Load empty bits into the next register for 64
+	# Load empty bits into the next register for 64
 	ldr		R1,=0 
 	bx		LR
 
@@ -24,7 +24,7 @@ Incr:
 
 	.global		Nested1
 Nested1:
-	// Call rand, add 1, return
+	# Call rand, add 1, return
 	push		{LR}
 	bl			rand
 	add			R0,R0,1
@@ -32,33 +32,33 @@ Nested1:
 
 	.global		Nested2
 Nested2:
-	// Call rand
-	push		{R4,LR}
+	# Call rand
+	push		{R2,LR}
 	bl			rand
-	// Store first rand in R4
-	mov			R4,R0
+	# Store first rand in R2
+	mov			R2,R0
 	bl			rand
-	// Add and return
-	add			R0,R0,R4
-	pop			{R4,PC}
+	# Add and return
+	add			R0,R0,R2
+	pop			{R2,PC}
 
 	.global   PrintTwo
 PrintTwo:
-	// R0 holds the str
-	// R1 holds the number
+	# R0 holds the str
+	# R1 holds the number
 	push	{R2,R3,LR}
 
 
-	// Store the input in R2 and R3
+	# Store the input in R2 and R3
 	mov		R2,R0
 	mov		R3,R1
 
-	// Call printf on R0, R1
+	# Call printf on R0, R1
 	bl		printf
 
-	// Move the str pointer back to R0
+	# Move the str pointer back to R0
 	mov		R0,R2
-	// Add 1 to the number in R3 and store it in R1
+	# Add 1 to the number in R3 and store it in R1
 	add		R1,R3,1
 
 	bl		printf
